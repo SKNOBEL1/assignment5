a533210766450152fef676797c419f22894be425
SKNOBEL1 <nobelrahman444@gmail.com>
2025-03-06 21:48:37 +0600

    i am failed

--- script/box.js
+++ script/box.js
@@ -3,6 +3,9 @@
 document.getElementById("box-1").addEventListener("click",
     function(event) {
         alert("Board updated successfully")
+          event.target.disabled = true
+    event.target.style.backgroundColor="red" 
+   
     }
 )
 
@@ -10,6 +13,8 @@ document.getElementById("box-1").addEventListener("click",
 document.getElementById("box-2").addEventListener("click",
     function(event) {
         alert("Board updated successfully")
+         event.target.disabled = true
+        event.target.style.backgroundColor="red"
     }
 )
 
@@ -17,6 +22,55 @@ document.getElementById("box-2").addEventListener("click",
 
 document.getElementById("box-3").addEventListener("click",
     function(event) {
-        alert("Board updated successfully")
+        alert("Board updated successfully") 
+        event.target.disabled = true
+        event.target.style.backgroundColor="red"
     }
 )
+// box-4
+
+document.getElementById("box-4").addEventListener("click",
+    function(event) {
+        alert("Board updated successfully")
+         event.target.disabled = true
+        event.target.style.backgroundColor="red"
+    }
+)
+// box-5
+
+document.getElementById("box-5").addEventListener("click",
+    function(event) {
+        alert("Board updated successfully")
+         event.target.disabled = true
+        event.target.style.backgroundColor="red"
+    }
+)
+// box-6
+
+document.getElementById("box-6").addEventListener("click",
+    function(event) {
+        alert("Board updated successfully")
+        event.target.disabled = true
+        event.target.style.backgroundColor="red"
+    }
+)
+
+
+// const quantityElements = document.querySelectorAll(".box-btn");
+// for(let btn of quantityElements){
+//     btn.addEventListener("click", function (event){
+//         console.log(event.target.innerText)
+
+//     });
+// }
+
+// document.querySelectorAll("box-btn").addEventListener("click",
+//     function (event) {
+//         const mainBalance = document.getElementById("main-balance").innerText;
+//         const convertedMainBalance = parseFloat(mainBalance);
+//         for('box-btn===1';){
+//             const sum = convertedMainBalance - box-btn;
+//             console.log(sum)
+//         }
+//     }
+// )
---
+++ script/history.js
@@ -0,0 +1,54 @@
+
+// document.getElementById(box-1).addEventListener(("click",
+//     function (event)
+// ))
+// box-1
+    document.getElementById("box-1-title").innerText;
+  const title1 = document.getElementById("history-part")  
+  const p = document.createElement("p");
+  
+  p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+  title1.appendChild(p) 
+
+//   box-2
+// document.getElementById("box-2-title").innerText;
+// const title2 = document.getElementById("history-part")  
+// const p = document.createElement("p");
+
+// p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+// title1.appendChild(p) 
+
+//   box-3
+
+// document.getElementById("box-3-title").innerText;
+// const title3 = document.getElementById("history-part")  
+// const p = document.createElement("p");
+
+// p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+// title1.appendChild(p)
+
+//   box-4
+
+// document.getElementById("box-4-title").innerText;
+// const title4 = document.getElementById("history-part")  
+// const p = document.createElement("p");
+
+// p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+// title1.appendChild(p) 
+
+//   box-5
+
+// document.getElementById("box-5-title").innerText;
+// const title5 = document.getElementById("history-part")  
+// const p = document.createElement("p");
+
+// p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+// title1.appendChild(p) 
+
+//   box-6
+// document.getElementById("box-6-title").innerText;
+// const title6 = document.getElementById("history-part")  
+// const p = document.createElement("p");
+
+// p.innerText= "You have Complete The Task Add Dark Mode at 12:48:15 PM"
+// title1.appendChild(p) 
